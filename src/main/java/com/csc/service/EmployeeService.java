c01a0fe2a9448ef8873a469df71dac8cf0d72ad6
harsh <singhharsh2177@gmail.com>
2021-02-10 22:16:52 +0530

    Feature Home Page

---
+++ src/main/java/com/csc/service/EmployeeService.java
@@ -0,0 +1,14 @@
+package com.csc.service;
+
+import com.csc.model.Employee;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+
+
+public interface EmployeeService {
+    List<Employee> getAllEmployees();
+    Page<Employee> findPaginated(int pageNo, int pageSize);
+    Page<Employee> findPaginated(int pageNo, int pageSize, String sortField, String sortDirection);
+}
