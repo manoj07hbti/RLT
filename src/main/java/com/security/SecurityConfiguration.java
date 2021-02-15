f9a427afb5790bd98c3891ffd611bf084e8b5494
dell <buntyraj568@gmail.com>
2021-02-15 17:57:28 -0500

    feature of login module

--- src/main/java/com/security/SecurityConfiguration.java
+++ src/main/java/com/security/SecurityConfiguration.java
@@ -22,65 +22,21 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     BCryptPasswordEncoder bCryptEncoder;
 
-   @Bean
-    public PasswordEncoder getPasswordEncoder() {return NoOpPasswordEncoder.getInstance();
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 
-    // in Memory Authentication  username and password for Application
- @Override
-    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-
-        authenticationManagerBuilder.inMemoryAuthentication()
-                .withUser("test")
-                            .password("test")
-                            .roles("USER")
-                .and()
-                .withUser("user")
-                            .password("pass")
-                            .roles("USER")
-                .and().withUser("adminuser")
-                            .password("admin")
-                            .roles("ADMIN");
-    }
-
-   /* // Setting Authorization : what user can do
+    // Database security
     @Override
-    protected void configure(HttpSecurity httpSecurity) throws Exception{
-        httpSecurity.authorizeRequests()
-                .antMatchers("/student_index").hasRole("ADMIN")
-                .antMatchers("/showNewStudentForm").hasRole("ADMIN")
-               // .antMatchers("/h2_consloe/**").permitAll()  // for h2 console
-                .antMatchers("/", "/**").permitAll()
-                .and()
-                .formLogin();
-
-
-        httpSecurity.csrf().disable();
-        httpSecurity.headers().frameOptions().disable();
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .usersByUsernameQuery("select username, password, enabled " +
+                        "from user_accounts1 where username = ?")
+                .authoritiesByUsernameQuery("select username, role " +
+                        "from user_accounts1 where username = ?")
+                .dataSource(dataSource)
+                .passwordEncoder(bCryptEncoder);
 
     }
-*/
- // with H2 database
-   /* @Override
-    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-
-        authenticationManagerBuilder.jdbcAuthentication().dataSource(dataSource)
-                .withDefaultSchema()
-                .withUser("test")
-                .password("test")
-                .roles("USER")
-                .and()
-                .withUser("user")
-                .password("pass")
-                .roles("USER")
-                .and().withUser("adminuser")
-                .password("admin")
-                .roles("ADMIN");
-    }
-*/
-
-
-
-
-
 }
